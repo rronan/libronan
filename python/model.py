13c7380101194c1789b1f477a3f5ad6ef134ab8d
rronan <riochet@hotmail.com>
2019-07-10 11:32:45 +0200

    update python

--- python/model.py
+++ python/model.py
@@ -8,28 +8,31 @@ from torchviz import make_dot, make_dot_from_trace
 
 
 class Model(object):
-
     def __init__(self):
         super().__init__()
-        __name__ = 'model'
-        self.core_module = None
+        __name__ = "model"
         self.is_gpu = False
         self.is_data_parallel = False
 
     def set_optim(self, args):
         self.grad_clip = args.grad_clip
         self.optimizer = vars(optim)[args.optimizer](
-            self.core_module.parameters(),
-            lr=args.lr,
-            weight_decay=args.weight_decay
+            self.core_module.parameters(), lr=args.lr, weight_decay=args.weight_decay
         )
         self.scheduler = ReduceLROnPlateau(
             self.optimizer,
             patience=args.lr_patience,
             factor=args.lr_decay,
             verbose=True,
-            eps=1e-9
+            eps=1e-9,
         )
+
+    def update(self, loss):
+        self.core_module.zero_grad()
+        loss.backward()
+        if self.grad_clip:
+            clip_grad_norm_(self.core_module.parameters, self.grad_clip)
+        self.optimizer.step()
 
     def step(self, batch, set_):
         x, y = batch[0], batch[1]
@@ -38,34 +41,30 @@ class Model(object):
             y = y.cuda()
         pred = self.core_module(x)
         loss = self.loss.forward(pred, y)
-        if set_ == 'train':
-            self.zero_grad()
-            loss.backward()
-            if self.grad_clip:
-                clip_grad_norm_(self.parameters, self.grad_clip)
-            self.optimizer.step()
-        return {'loss': loss.data.item()}
+        if set_ == "train":
+            self.update(loss)
+        return {"loss": loss.data.item()}
 
     def load(self, path):
-        print(f'loading {path}')
+        print(f"loading {path}")
         state_dict = torch.load(path, map_location=lambda storage, loc: storage)
         try:
             self.core_module.load_state_dict(state_dict)
         except RuntimeError as e:
             print(e)
             state_dict = OrderedDict(
-                {k.replace("module.", "", 1):v for k,v in state_dict.items()}
+                {k.replace("module.", "", 1): v for k, v in state_dict.items()}
             )
             self.core_module.load_state_dict(state_dict)
 
     def save(self, path, epoch):
-        with open(path / f'{self.__name__}.txt', 'w') as f:
+        with open(path / f"{self.__name__}.txt", "w") as f:
             f.write(str(self))
         if self.is_data_parallel:
             state_dict = self.core_module.module.state_dict()
         else:
             state_dict = self.core_module.state_dict()
-        torch.save(state_dict, path / f'weights_{epoch}.pth')
+        torch.save(state_dict, path / f"weights_{epoch}.pth")
 
     def gpu(self):
         self.core_module.cuda()
@@ -76,7 +75,7 @@ class Model(object):
         self.is_data_parallel = True
 
     def get_lr(self):
-        return self.optimizer.param_groups[0]['lr']
+        return self.optimizer.param_groups[0]["lr"]
 
     def get_num_parameters(self):
         return sum([m.numel() for m in self.core_module.parameters()])
@@ -84,11 +83,8 @@ class Model(object):
     def make_dot(self, *x):
         if self.is_gpu:
             x = [xx.cuda() for xx in x]
-        dot = make_dot(
-            self(*x),
-            params=dict(self.core_module.named_parameters())
-        )
-        dot.render('dot', view=False)
+        dot = make_dot(self(*x), params=dict(self.core_module.named_parameters()))
+        dot.render("dot", view=False)
 
     def make_dot_from_trace(self, *x):
         if self.is_gpu:
@@ -96,11 +92,10 @@ class Model(object):
         with torch.onnx.set_training(self.core_module, False):
             trace, _ = torch.jit.get_trace_graph(self.core_module, args=(*x,))
         dot = make_dot_from_trace(trace)
-        dot.render('dot_from_trace', view=False)
+        dot.render("dot_from_trace", view=False)
 
     def output(self):
         raise NotImplementedError
 
     def __call__(self, x):
         return self.core_module.forward(x)
-
